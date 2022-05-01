5ce63ee3cb7a4b184bd5ec1a500e397718c94465
koske0125 <79013492+koske0125@users.noreply.github.com>
2022-05-01 07:35:11 +0000

    [UPDATE]いいねボタンの非同期処理化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,14 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id]) #該当するbook_idのレコードをBookモデルから持ってくる
-    favorite = current_user.favorites.new(book_id: book.id)  #現在のユーザーに紐づく形でfavoritesモデルを新規作成 favoritesモデルのbook_idカラムにbook.idを格納
+    @book = Book.find(params[:book_id]) #該当するbook_idのレコードをBookモデルから持ってくる
+    favorite = current_user.favorites.new(book_id: @book.id)  #現在のユーザーに紐づく形でfavoritesモデルを新規作成 favoritesモデルのbook_idカラムにbook.idを格納
     favorite.save
-    redirect_back(fallback_location: books_path) #元の画面にリダイレクト 失敗時はbookindexへ
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id) #現在のユーザーのfavoritesモデルからbook.idに一致するbook_idが存在するレコードを格納する
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id) #現在のユーザーのfavoritesモデルからbook.idに一致するbook_idが存在するレコードを格納する
     favorite.destroy
-    redirect_back(fallback_location: books_path)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user
-  has_many :favorites
   has_many :book_comments
+  has_many :favorites, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
   def favorited_by?(user)
-    favorites.exists?(user_id:user.id)
+    favorites.exists?(user_id: user.id)
   end
 
   def self.looks(search, word)
