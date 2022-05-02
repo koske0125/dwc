f5eccb7fd6d8b30f217438ca2e625e0b6857459c
koske0125 <79013492+koske0125@users.noreply.github.com>
2022-05-02 07:31:58 +0000

    [Update]コメント機能の非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,12 +1,13 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id]) #book_idに該当するレコードを取得
+    @book = Book.find(params[:book_id]) #book_idに該当するレコードを取得
     comment = BookComment.new(post_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: book_path(book))
+    @book_comments = @book.book_comments
+    # redirect_back(fallback_location: book_path(book))
   end
 
   def destroy
